0402a65d09d5d908cc2d8bdfa7b8a8bec0b63296
Andrey <Jarerrag@gmail.com>
2020-02-05 00:22:23 +0300

    Updates

---
+++ precache-manifest.14430d6342b1b76ca88b326673a95ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcb5c00c8d412661f90e6bd8f3c6612",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "db7a0039623cd9db08c2",
+    "url": "/Todo/static/css/main.a6892cbf.chunk.css"
+  },
+  {
+    "revision": "7a3785bf7336c0432fb6",
+    "url": "/Todo/static/js/2.39a5c75c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo/static/js/2.39a5c75c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db7a0039623cd9db08c2",
+    "url": "/Todo/static/js/main.afad89b0.chunk.js"
+  },
+  {
+    "revision": "7d53a05980f3091c9d0c",
+    "url": "/Todo/static/js/runtime-main.d4730ea3.js"
+  }
+]);
